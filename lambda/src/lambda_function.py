6363062d94940ee767f4e4220aeaf44a7e0278ca
Austin Lawless <austindlawless@gmail.com>
2017-08-19 14:55:08 -0500

    [MASTER] now getting the correct next game

--- lambda/src/lambda_function.py
+++ lambda/src/lambda_function.py
@@ -2,8 +2,8 @@
 from __future__ import print_function
 
 import boto3
-import random
 from datetime import datetime
+from boto3.dynamodb.conditions import Key
 
 # --------------- Globals ------------------------------------------------------
 
@@ -81,25 +81,47 @@ def handle_session_end_request():
         card_title, speech_output, None, should_end_session))
 
 
-def get_next_game():
+def get_current_season():
     seasons_table = boto3.resource('dynamodb').Table('cudas_schedule')
-    seasons = seasons_table.scan()['Items']
-    season = seasons[0]
-    games = season['games']
-    game = games[0]
+    current_season = seasons_table.scan(
+        FilterExpression=Key('currentSeason').eq(True)
+    )['Items'][0]
+    print("season_id=" + current_season['season'])
+    print("season_currentSeason=" + str(current_season['currentSeason']))
+    return current_season
+
+
+def get_next_game_datetime_from(current_season):
+    print("Getting next game from current season.")
+    next_game_datetime = None
+    current_date = datetime.today()
+    print("Current datetime=" + str(current_date))
+    games = current_season['games']
+    for game in games:
+        print("Checking game with date=" + game['date'])
+        status = game['status']
+        print("Status=" + status)
+        if status is not None and status == "upcoming":
+            game_datetime = datetime_from_game(game)
+            if current_date < game_datetime and (next_game_datetime is None or game_datetime < next_game_datetime):
+                print("updating next_game_datetime to " + str(game_datetime))
+                next_game_datetime = game_datetime
+    return next_game_datetime
+
+
+def datetime_from_game(game):
     return datetime.strptime(game['date'] + "-" + game['time'] + "m", "%m-%d-%Y-%I:%M%p")
 
 
 def get_next_game_text():
-    game_datetime = get_next_game()
-    current_date = datetime.today()
-    if current_date > game_datetime:
+    next_game_datetime = get_next_game_datetime_from(get_current_season())
+    if next_game_datetime is None:
         return "I don't see a next game on the calendar."
     else:
-        return "The next game is on " + str(game_datetime.date()) + " at " + str(game_datetime.time())
+        return "The next game is on " + str(next_game_datetime.date()) + " at " + str(next_game_datetime.time())
 
 
-def get_random_note_response():
+def get_next_game_response():
     card_title = "Cudas: Next Game"
     session_attributes = {}
     should_end_session = True
@@ -142,7 +164,7 @@ def on_intent(intent_request, session):
 
     # Dispatch to your skill's intent handlers
     if intent_name == "NextGame":
-        return get_random_note_response()
+        return get_next_game_response()
     elif intent_name == "AMAZON.HelpIntent":
         return get_welcome_response()
     elif intent_name == "AMAZON.CancelIntent" or intent_name == "AMAZON.StopIntent":
